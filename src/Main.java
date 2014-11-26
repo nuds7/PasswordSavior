71190dc14abd37657ee873a18e7f06332495f8e1
Elizabwth <lizabwth@gmail.com>
2014-11-26 15:51:38 -0800

    Revert "updated to newest version"
    This reverts commit c2310d8a585062d8b7e1ae540184c5a671a8d3c2.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
 import java.io.IOException;
 
 import javax.swing.*;
-
 public class Main {
 	public static void main(String[] args) throws IOException{
 		JFrame frame = new JFrame ("Password Savior");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 	
 		JTabbedPane tp = new JTabbedPane();
-		PasswordPanel pwdPanel = new PasswordPanel();
-		GeneratorPanel genPanel = new GeneratorPanel(pwdPanel);
-		
-		tp.addTab ("Passwords", pwdPanel);
-		tp.addTab ("Generator", genPanel);
-		
+		tp.addTab ("Passwords", new PasswordsPanel());
+		tp.addTab ("Generator", new PasswordsPanel());
 		frame.getContentPane().add(tp);
 		frame.pack();
-		
-		frame.setSize(640,360);
-		frame.setResizable(false);
 		frame.setVisible(true);
-		frame.setLocationRelativeTo( null );
 	}
 }
